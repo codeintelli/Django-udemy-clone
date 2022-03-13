8aef15da55d6c00d34d0bc0dc3f713c9c26810ae
codeintelli <codeintelli2021@gmail.com>
2022-03-14 00:21:03 +0530

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from accounts.managers import UserManager
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    objects = UserManager()
